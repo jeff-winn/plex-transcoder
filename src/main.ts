9bd89f3dc3505e8389ccdf56c30d9bfbb41a95cc
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-06-06 10:59:55 -0400

    Adding cors dependency.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/api/transcode', (req, res) => {
